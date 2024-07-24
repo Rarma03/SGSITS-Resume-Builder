bf55b93f9e9e8678967131cf4621e7199552c62c
Rarma03 <raj0701verma@gmail.com>
2024-07-25 01:11:45 +0530

    Update

--- api/index.js
+++ api/index.js
@@ -30,7 +30,8 @@ app.use(cookieParser());
 
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:5173'
+    methods: ['POST', 'GET'],
+    origin: ['http://localhost:5173', 'https://sgsits-resume-builder.vercel.app']
 }))
 
 // mongoose.connect(process.env.MONGO_URL)
