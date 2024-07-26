4c8ef5e1bf157019a8e6cbbfd781f0149ac7f6b5
Rarma03 <raj0701verma@gmail.com>
2024-07-26 08:14:15 +0530

    Update loginPage

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -27,6 +27,13 @@ export default function LoginPage() {
     }
     return (
         <div className="bg-black text-white h-[100vh]">
+            <div className="pt-4 pl-4 -mb-4">
+                <Link to={'/'} className="hover:bg-white hover:text-black flex  w-6 rounded-full h-6">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="m11.25 9-3 3m0 0 3 3m-3-3h7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                    </svg>
+                </Link>
+            </div>
 
             <div className="grow flex items-center justify-around">
                 <div className="mb-32 mt-[30vh] p-5">
